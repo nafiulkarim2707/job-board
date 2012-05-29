0af4da6901b1746fd116f006a8539fee5dffb2ff
HungryCoder <rajuru@gmail.com>
2012-05-29 14:33:50 +0600

    project intialized disabled activerecord enabled mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard::Application.config.secret_token = '1d36c0de1aa4ddd1fdf8798d68d1bd3f1302afc6c0e9fe450ce7afdffe7515bc83f48823b4282f146e6ad5b07f0fb20d829d27e1d38507937101c8499f2690dd'
