16d7ad74bcae9b9b74414aa037a3d7a40f2185b2
HungryCoder <rajuru@gmail.com>
2012-06-05 10:55:31 +0600

    asset serving issue fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,14 @@ JobBoard::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
+  #@todo need to set to false when the apache issue is fixed
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +47,7 @@ JobBoard::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js console/console.js application.css )
+  config.assets.precompile += %w( application.js console/console.js application.css widget.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
