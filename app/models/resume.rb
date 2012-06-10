68f165e43759443cb9582b4cd1104c7794b9f0c0
HungryCoder <rajuru@gmail.com>
2012-06-10 23:33:17 +0600

    resume extended

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -6,8 +6,11 @@ class Resume
   field :firstname, type: String
   field :lastname, type: String
   field :email, type: String
+  field :website, type: String
+  field :mailing_address, type: String
+  field :contact_number, type: String
+  field :summary, type: String
   field :language, type: String
-  field :skills, type: String
 
   validates :firstname, :presence => true
   validates :lastname, :presence => true
@@ -16,9 +19,12 @@ class Resume
   belongs_to :candidate, :class_name => 'User'
   embeds_many :positions
   embeds_many :educations
+  embeds_many :skills
+  has_many :candidacies
 
   accepts_nested_attributes_for :positions, :allow_destroy => true, :reject_if => proc {|p| p[:title].blank?}
-  accepts_nested_attributes_for :educations, :allow_destroy => true, :reject_if => proc {|p| p[:school].blank?}
+  accepts_nested_attributes_for :educations, :allow_destroy => true, :reject_if => proc {|e| e[:school].blank?}
+  accepts_nested_attributes_for :skills, :allow_destroy => true, :reject_if => proc {|s| s[:type].blank?}
 
-  has_many :candidacies
+
 end
